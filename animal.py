f44820e82335faf43140d2c0746c06ec879b8e7f
kalangijayakody <kalangijayakody1002@gmail.com>
2024-02-16 15:12:41 +0530

    dog function added

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def dog():
+    print("Baw")
+
 def default():
     print("Hello")
 
 def main():
-    default()
+    if sys.argv[1]=="dog":
+        dog()
+    else:
+        default()
     
 if __name__ =="__main__":
     main()
