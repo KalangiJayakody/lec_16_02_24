2ed26f408816ed0da3f3a0cce0d99863d5c7041e
kalangijayakody <kalangijayakody1002@gmail.com>
2024-02-16 15:00:25 +0530

    cat function added

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def cat():
+    print("Meow")
+
 def default():
     print("Hello")
 
 def main():
-    default()
+    if sys.argv[1]=="cat":
+        cat()
+    else:
+        default()
     
 if __name__ =="__main__":
     main()
